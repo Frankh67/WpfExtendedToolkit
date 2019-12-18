28f8c770b623ddcb4f41f2758f91027910b30fad
Frankh67 <47349495+Frankh67@users.noreply.github.com>
2019-12-18 12:51:15 +0100

    Missing using added.
    Added using for enum DependencyProperty.

--- Src/Xceed.Wpf.Toolkit/BusyIndicator/Implementation/Converters/ProgressBarWidthConverter.cs
+++ Src/Xceed.Wpf.Toolkit/BusyIndicator/Implementation/Converters/ProgressBarWidthConverter.cs
@@ -19,6 +19,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Xceed.Wpf.Toolkit.Converters

6fe6d281ca9758b84a20c9350104bb9ae27dd8d8
Frank Hablizel <Frank.Hablizel.Partner@alpine.de>
2019-12-19 10:20:04 +0100

    Package name and properties adjusted.

--- Src/Xceed.Wpf.Toolkit/Properties/AssemblyInfo.cs
+++ Src/Xceed.Wpf.Toolkit/Properties/AssemblyInfo.cs
@@ -31,7 +31,6 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("This assembly implements various Windows Presentation Framework controls.")]
 
 [assembly: AssemblyCopyright( "" )]
 [assembly: AssemblyCulture( "" )]
